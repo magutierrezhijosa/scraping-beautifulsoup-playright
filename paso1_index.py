46863416932fbda17103b01ad456df42aab22e93
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-20 19:10:02 +0100

    Desarrollando paso1.py probando BeautifulSoup

--- paso1_index.py
+++ paso1_index.py
@@ -6,3 +6,11 @@ from bs4 import BeautifulSoup
 # Importamos las CONSTANTES
 from config import URL_TO_SCRAP,BASE_URL
 
+# Paso 1 : Descargar la pagina
+# request.get() hace una peticion HTTP GET - como cuando tu navegador carga la pagina
+respuesta = requests.get(URL_TO_SCRAP)
+
+# Verificamos que fue bien (codigo 200 = OK)
+print(f"Codigo de respuesta:{respuesta.status_code}")
+
+# Paso 2: Parsear el HTML con BeautifulSoup
